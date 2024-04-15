ff5d0cf04f0e9069547766635b3249bfdded252f
Dayra Leon <dayraleon@dayras-mbp.laverne.local>
2024-04-15 14:08:57 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 #install nltk
 #install textblob
 #install newspaper 3k
+
+from textblob import TextBlob
+from newspaper import Article
+
+#url ="https://en.wikipedia.org/wiki/Mathematics" positive
+#url = "https://www.foxla.com/news/pomona-crash-3-women-killed" negative
+url = "https://www.hopkinsmedicine.org/health/wellness-and-prevention/the-power-of-positive-thinking"
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+text = article.summary
+print(text)
+
+blob = TextBlob(text)
+sentiment = blob.sentiment.polarity #-1 to 1
+print(sentiment)
